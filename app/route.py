ee76d832babb92c2ad42f3dd0d25f8449cb6ad6d
dozie-oc <chidozieoriaku@gmail.com>
2026-06-08 16:53:09 +0100

    model update and vibes

--- app/route.py
+++ app/route.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flask import request, redirect, render_template, url_for
 from app.forms import LinkForm
 from. import app
 
@@ -7,6 +7,7 @@ def home():
     form = LinkForm()
     if form.validate_on_submit():
         url = form.url.data
+
         # Here you would add logic to shorten the URL and save it to a database
         return redirect(url)  # For now, just redirect to the original URL
     return render_template('base.html', form=form)
