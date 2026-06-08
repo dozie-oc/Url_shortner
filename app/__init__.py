3707be749cbfad282e3c5fbc5f4eabbba028fc75
dozie-oc <chidozieoriaku@gmail.com>
2026-06-08 09:28:16 +0100

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+#db = SQLAlchemy(app)
+
+def create_app():
+    app.config['SECRET_KEY'] = 'your_secret_key_here'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.register_blueprint(route)
+    #db.init_app(app)
+    
+    #db.create_all()
+    from app import route, forms
+    return app
---
+++ app/route.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, redirect, render_template, url_for
+from app.forms import LinkForm
+from. import app
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    form = LinkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        # Here you would add logic to shorten the URL and save it to a database
+        return redirect(url)  # For now, just redirect to the original URL
+    return render_template('base.html', form=form)
