4a8867945c2ef066641067983c055c210fa1ad2c
dozie-oc <chidozieoriaku@gmail.com>
2026-06-10 21:19:25 +0100

    requirements update

--- main/pages.py
+++ main/pages.py
@@ -6,11 +6,11 @@ pages =  Blueprint('pages', __name__)
 @pages.route('/', methods=['GET', 'POST'])
 def index():
     form = LinkForm()
+    url = None
     if form.validate_on_submit():
         url = form.url.data
-        if request.method == "POST":
-            # Here you would typically shorten the URL and save it to the database
-            pass
+        # Here you would typically shorten the URL and save it to the database
+        
     return render_template('index.html', form=form, short_url=url)
 
 @pages.route('/traffic')
