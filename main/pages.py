e4c3f916d9baffd2ef1aacf21e5eb928270cbc80
dozie-oc <chidozieoriaku@gmail.com>
2026-06-26 17:05:24 +0100

    Implement URL deletion functionality and enhance user feedback with flash messages, as well as fix the url click count that wasn't registering at all due to model mix up

--- main/pages.py
+++ main/pages.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 from .forms import LinkForm
 from .models import click, url, short_code
 from hashids import Hashids
 from . import db
 import logging
-import datetime
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,11 +13,24 @@ hashid = Hashids(min_length=6)
 
 pages =  Blueprint('pages', __name__)
 
-def log_click(short_code):
-    short_code_entry = short_code.query.filter_by(code=code).first()
+def log_click(entry):
+    short_code_entry = short_code.query.filter_by(code=entry).first()
     if short_code_entry:
         click_entry = click(short_code_id=short_code_entry.id, timestamp=datetime.utcnow())
         db.session.add(click_entry)
+
+def delete_url(url_id):
+    url_entry = url.query.get(url_id)
+    if url_entry:
+        # Delete associated short codes and clicks
+        for sc in url_entry.short_codes:
+            click.query.filter_by(short_code_id=sc.id).delete()
+            db.session.delete(sc)
+        db.session.delete(url_entry)
+        db.session.commit()
+        logger.info(f"Deleted URL with ID: {url_id}")
+    else:
+        logger.warning(f"URL with ID: {url_id} not found for deletion.")
 
 @pages.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,6 +54,7 @@ def index():
             db.session.commit()
 
         short_url = url_for("pages.redirect_to_url", short_code=hashed_id, _external=True)
+        flash("URL shortened successfully!", "success")
     
     return render_template('index.html', form=form, short_url=short_url or None)
 
@@ -55,7 +69,10 @@ def redirect_to_url(short_code):
             return redirect(original_url.url)
     return "<h1>URL not found</h1>"
 
-@pages.route('/traffic')
+@pages.route('/traffic', methods=['GET', 'POST'])
 def traffic():
+    if request.method == 'POST':
+        url_id = request.form.get('url_id')
+        delete_url(url_id)
     urls = url.query.all()
     return render_template('analytics.html', urls=urls)
